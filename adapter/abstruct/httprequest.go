00e881688656a961dbaee0b2b92c84ae39e82d4e
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-02-14 16:42:22 +0900

    feat: 🎸 http post method add

--- adapter/abstruct/httprequest.go
+++ adapter/abstruct/httprequest.go
@@ -4,6 +4,7 @@ import "github.com/Siroyaka/dotschedule-backend_v2/utility"
 
 type HTTPRequest interface {
 	Get(string) (HTTPResponse, utility.IError)
+	Post(HTTPPostParams) (HTTPResponse, utility.IError)
 	SetTimeout(int)
 }
 
@@ -11,4 +12,10 @@ type HTTPResponse interface {
 	Status() string
 	StatusCode() int
 	Body() string
+}
+
+type HTTPPostParams interface {
+	ContentType() string
+	Url() string
+	Content() string
 }
