ab60ea10d66815376bc856b41c40d2b67a93a6d3
siroyaka <29934865+Siroyaka@users.noreply.github.com>
2023-02-21 19:46:56 +0900

    refactor: 💡 不要になったファイルを削除

--- usecase/abstruct/fullschedule/update.go
+++
@@ -1,9 +0,0 @@
-package fullschedule
-
-import (
-	"github.com/Siroyaka/dotschedule-backend_v2/utility"
-)
-
-type UpdateAnyColumnRepository interface {
-	Update(utility.WrappedTime, ...any) (int64, utility.IError)
-}
